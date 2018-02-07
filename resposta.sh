fae93ee2cb09e32f11b3f20a4a37aca81a7df4f2
veruskasantos <veruska.santos@ccc.ufcg.edu.br>
2018-02-07 20:43:52 -0300

    Adicionando os outros comandos.

--- resposta.sh
+++ resposta.sh
@@ -29,16 +29,28 @@ npm install d3-dsv
 #convertendo o arquivo com os dados do censo para ndjson
 ./node_modules/d3-dsv/bin/dsv2json -r ';' -n < Responsavel01_PB.csv > pb-censo.ndjson
 
+#VOLTAR: O QUE EH ISSO? CADE ESSES ATRIBUTOS?
+#transformando arquivo ndjson, adicionando chave Cod_setor
+./node_modules/ndjson-cli/ndjson-map 'd.Cod_setor = d.properties.CD_GEOCODI, d' < pb-ortho.ndjson > saida-ortho-sector.ndjson
 
-#transformando o arquivo ndjson
+#mesclando arquivos dos dados ao mapa
+./node_modules/ndjson-cli/ndjson-join 'd.Cod_setor' saida-ortho-sector.ndjson pb-censo.ndjson > censo-mapa.ndjson
 
-#mesclando arquivos com os dados ao mapa
+#organizando o arquivo e deixando um objeto por linha
+./node_modules/ndjson-cli/ndjson-map 'd[0].properties = {renda: Number(d[1].V005.replace(",", "."))}, d[0]' < [ndjson-depois-do-join]] > pb-ortho-comdado.ndjson
 
+npm install -g topojson
 
+#compactando o arquivo
+./node_modules/topojson/geo2topo -n tracts=pb-ortho-comdado.ndjson > pb-tracts-topo.json
 
-responsavel 1, mulheres responsaveis pelo domicilio
+#simplificando ainda mais e quantizando a geometri do json
+./node_modules/topojson/toposimplify -p 1 -f < pb-tracts-topo.json | topoquantize 1e5 > pb-quantized-topo.json
 
+npm install -g d3
+npm install -g d3-scale-chromatic
 
+#gerando <FINALMENTE> o gráfico svg
+topo2geo tracts=- < pb-quantized-topo.json | ndjson-map -r d3 'z = d3.scaleSequential(d3.interpolateViridis).domain([0, 1e3]), d.features.forEach(f => f.properties.fill = z(f.properties.renda)), d' | ndjson-split 'd.features' | geo2svg -n --stroke none -w 1000 -h 600 > pb-tracts-threshold-light.svg
 
-
-
+#</end>
